5aae7c2d6bd211f99d12d028726c826cb42ec5bd
niki17290 <78669529+niki17290@users.noreply.github.com>
2021-06-27 21:13:32 +0100

    Update script.js

--- resources/js/script.js
+++ resources/js/script.js
@@ -1,4 +1,4 @@
 document.getElementById("new").addEventListener('click', function(){
-	document.querySelector('.contact').innerHTML = "<h2>Contact</h2> <br> <p> <b>Email:</b> niki17290@gmail.com <br><b>Contact:</b> 07769449764<br></p>";
+	document.querySelector('.contact').innerHTML = "<h2>Contact</h2> <br> <p> <b>Email:</b>  <br><b>Contact:</b> <br></p>";
 
 });
